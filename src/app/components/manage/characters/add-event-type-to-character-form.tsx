e5db0b25b4044852d02b94c917a10da0b7a6e2c8
Meatclump <henrik.persson.1988@proton.me>
2024-04-01 12:06:15 -0500

    Removed extra step from adding event to character

--- src/app/components/manage/characters/add-event-type-to-character-form.tsx
+++ src/app/components/manage/characters/add-event-type-to-character-form.tsx
@@ -1,79 +1,65 @@
 'use client'
 
 import { Character, Prisma } from "@prisma/client"
-import { ChangeEvent, useState, useTransition } from "react"
-import { FaPlus } from "react-icons/fa6"
-import { connectEventToCharacter, getEventTypes } from "@/app/actions"
+import { ChangeEvent, useTransition } from "react"
+import { connectEventToCharacter } from "@/app/actions"
 
 interface AddEventTypeToCharacterFormProps {
 	character: Character
-}
-
-export const AddEventTypeToCharacterForm = ({
-	character
-}: AddEventTypeToCharacterFormProps) => {
-	const [showMenu, setShowMenu] = useState(false)
-	const [eventTypes, setEventTypes] = useState<Prisma.EventTypeGetPayload<{
+	eventTypes: Prisma.EventTypeGetPayload<{
 		include: {
 			color: true
 		}
-	}>[]>([])
+	}>[]
+}
+
+export const AddEventTypeToCharacterForm = ({
+	character,
+	eventTypes
+}: AddEventTypeToCharacterFormProps) => {
 	const [isPending, startTransition] = useTransition()
 
-	const handleClick = async () => {
-		const eventTypes = await getEventTypes()
-		if (eventTypes && eventTypes.length > 0) {
-			setEventTypes(eventTypes)
-		}
-		setShowMenu(prevState => !prevState)
-	}
-
 	const handleChange = async (e: ChangeEvent<HTMLSelectElement>) => {
-		setShowMenu(false)
-		if (e.target.value === "") return
-		const selectedEventType = eventTypes.filter(type => type.id === e.target.value)[0]
-		const result = await connectEventToCharacter(character.id, selectedEventType.id)
+		startTransition(async () => {
+			if (eventTypes && eventTypes.length > 0) {
+				
+				if (e.target.value === "") return
+				const selectedEventType = eventTypes.filter(type => type.id === e.target.value)[0]
+				const result = await connectEventToCharacter(character.id, selectedEventType.id)
+				if (result) {
+					e.target.value = ""
+				}
+			}
+		})
 	}
 
 	return (
-		<div className="flex gap-1 relative">
-			<button
-				className="block hover:scale-110 bg-green-600 px-2 rounded-md disabled:bg-slate-100/20"
+		<form className="z-10 inline-block ms-1 mt-1">
+			<select
+				className="px-1 rounded-md p-1 border text-slate-900 text-sm max-w-[65px]"
+				onChange={handleChange}
+				defaultValue=""
 				disabled={isPending}
-				onClick={() => {
-					if (isPending) return
-					startTransition(() => {
-						handleClick()
-					})
-				}}
 			>
-				<FaPlus className="text-green-950 text-sm rounded-sm" />
-			</button>
-			{
-				showMenu &&
-				<form className="absolute bottom-[-30px] text-slate-900 w-[200px] z-10">
-					<select className="px-1 rounded-md w-full p-1 border bg-gray-300" onChange={handleChange} defaultValue="">
+				<option
+					className="font-sans font-medium"
+					value=""
+					disabled
+				>
+					Add...
+				</option>
+				{
+					eventTypes.map(type => (
 						<option
-							className="font-sans font-medium"
-							value=""
-							disabled
+							className={`font-sans font-medium ${type.color.bgCSS}`}
+							key={type.id}
+							value={type.id}
 						>
-							Choose Type...
+							{type.name}
 						</option>
-						{
-							eventTypes.map(type => (
-								<option
-									className={`font-sans font-medium ${type.color.bgCSS}`}
-									key={type.id}
-									value={type.id}
-								>
-									{type.name}
-								</option>
-							))
-						}
-					</select>
-				</form>
-			}
-		</div>
+					))
+				}
+			</select>
+		</form>
 	)
 }

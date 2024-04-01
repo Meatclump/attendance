459fb80f38a0ac552548ff43a04604f99eef9b94
Meatclump <henrik.persson.1988@proton.me>
2024-04-01 09:18:21 -0500

    move roster header section into card header component

--- src/app/components/manage/characters/character-roster-panel.tsx
+++ src/app/components/manage/characters/character-roster-panel.tsx
@@ -1,38 +1,16 @@
-import Link from "next/link"
 import { Suspense } from "react"
 import { AddCharacterForm } from "./add-character-form"
 import { RosterTable } from "./roster-table"
 
 interface RosterPageProps {
-	searchParams: { [key: string]: string | string[] | undefined }
+	showInactive: boolean
 }
 
 export const CharacterRosterPanel = ({
-	searchParams
+	showInactive
 }: RosterPageProps) => {
-	const params = searchParams
-	const showInactive = params.tab === "inactive"
 	return (
 		<>
-			<div className="flex items-center gap-4 p-4">
-				<h2 className="flex-1 py-1">
-					Character Roster
-				</h2>
-				<div className="flex gap-4 justify-end">
-					<Link
-						className={`${!showInactive && "bg-slate-100 text-slate-900"} border rounded-lg px-2 py-1`}
-						href="?tab=active"
-					>
-						Active
-					</Link>
-					<Link
-						className={`${showInactive && "bg-slate-100 text-slate-900"} border rounded-lg px-2 py-1`}
-						href="?tab=inactive"
-					>
-						Inactive
-					</Link>
-				</div>
-			</div>
 			<div className="flex-1 border-t border-t-slate-100/20 pt-4 overflow-y-scroll">
 				{
 					showInactive

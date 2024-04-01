9d5ffcdd3c580515e856f2483bc74be765ca8b29
Meatclump <henrik.persson.1988@proton.me>
2024-04-01 09:18:00 -0500

    rewrite manage page to utilize card components

--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -1,5 +1,8 @@
 import { CharacterRosterPanel } from "@/app/components/manage/characters/character-roster-panel"
 import { EventTypePanel } from "@/app/components/manage/events/event-type-panel"
+import { CardWrapper } from "../components/card/card-wrapper"
+import { CardHeader } from "../components/card/card-header"
+import Link from "next/link"
 
 interface RosterPageProps {
 	searchParams: { [key: string]: string | string[] | undefined }
@@ -8,18 +11,38 @@ interface RosterPageProps {
 const RosterPage = ({
 	searchParams
 }: RosterPageProps) => {
+	const params = searchParams
+	const showInactive = params.tab === "inactive"
 	return (
 		<>
-			<div className="flex flex-1 overflow-y-scroll">
-				<div className="rounded-xl bg-slate-100/10 border border-slate-100/20 flex flex-col mx-auto w-full">
-					<CharacterRosterPanel searchParams={searchParams} />
-				</div>
-			</div>
-			<div className="flex flex-1 ">
-				<div className="rounded-xl bg-slate-100/10 border border-slate-100/20 flex flex-col mx-auto w-full">
-					<EventTypePanel />
-				</div>
-			</div>
+			<CardWrapper>
+				<CardHeader>
+					<h2 className="flex-1 py-1">
+						Character Roster
+					</h2>
+					<div className="flex gap-4 justify-end">
+						<Link
+							className={`${!showInactive && "bg-slate-100 text-slate-900"} border rounded-lg px-2 py-1`}
+							href="?tab=active"
+						>
+							Active
+						</Link>
+						<Link
+							className={`${showInactive && "bg-slate-100 text-slate-900"} border rounded-lg px-2 py-1`}
+							href="?tab=inactive"
+						>
+							Inactive
+						</Link>
+					</div>
+				</CardHeader>
+				<CharacterRosterPanel showInactive={showInactive} />
+			</CardWrapper>
+			<CardWrapper>
+				<CardHeader>
+					<h2 className="py-1">Event Types</h2>
+				</CardHeader>
+				<EventTypePanel />
+			</CardWrapper>
 		</>
 	)
 }
